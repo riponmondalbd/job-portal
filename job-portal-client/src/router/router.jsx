1db81fff1aee490008e99ea32d21c91fda16d79e
riponmondalbd <riponmondal149@gmail.com>
2025-01-28 10:45:08 +0600

    auth route setup and folder structure

--- job-portal-client/src/router/router.jsx
+++ job-portal-client/src/router/router.jsx
@@ -1,9 +1,23 @@
 import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layout/MainLayout";
+import Home from "../pages/Home/Home";
+import Register from "../pages/Register/Register";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello dunia</div>,
+    element: <MainLayout />,
+    errorElement: <h2>Route not found</h2>,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+    ],
   },
 ]);
 
