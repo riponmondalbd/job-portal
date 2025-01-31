54fa76eef7d1f7d94a8fa9421f9203034aa2ec8f
riponmondalbd <riponmondal149@gmail.com>
2025-01-31 09:38:28 +0600

    use custom hook and create job application job api

--- job-portal-client/src/pages/JobApply/JobApply.jsx
+++ job-portal-client/src/pages/JobApply/JobApply.jsx
@@ -1,8 +1,10 @@
 import { useParams } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
 
 const JobApply = () => {
   const { id } = useParams();
-  console.log(id);
+  const { user } = useAuth();
+  console.log(id, user);
 
   const submitJobApplication = (e) => {
     e.preventDefault();
@@ -12,47 +14,48 @@ const JobApply = () => {
     const resume = form.resume.value;
 
     console.log(linkedIn, github, resume);
+
+    const jobApplication = {
+      job_id: id,
+      applicant_email: user.email,
+      linkedIn,
+      github,
+      resume,
+    };
   };
 
   return (
-    <div className="hero bg-base-200 min-h-screen">
-      <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="text-center lg:text-left">
-          <h1 className="text-5xl font-bold">Apply now!</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-        </div>
-        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-          <form onSubmit={submitJobApplication} className="card-body">
-            <fieldset className="fieldset">
-              <label className="fieldset-label">LinkedIn URl</label>
-              <input
-                type="url"
-                name="linkedIn"
-                className="input"
-                placeholder="LinkedIn URL"
-              />
-              <label className="fieldset-label">Github URL</label>
-              <input
-                type="url"
-                name="github"
-                className="input"
-                placeholder="Github URL"
-              />
-              <label className="fieldset-label">Resume URL</label>
-              <input
-                type="url"
-                name="resume"
-                className="input"
-                placeholder="Resume URL"
-              />
-              <button className="btn btn-neutral mt-4">Apply</button>
-            </fieldset>
-          </form>
-        </div>
+    <div className="min-h-[70vh] hero">
+      <div className="card bg-base-100 w-full shadow-2xl pt-8">
+        <h1 className="text-5xl font-bold text-center">
+          Apply Job and Good Luck!
+        </h1>
+        <form onSubmit={submitJobApplication} className="card-body">
+          <fieldset className="fieldset">
+            <label className="fieldset-label">LinkedIn URl</label>
+            <input
+              type="url"
+              name="linkedIn"
+              className="input w-full"
+              placeholder="LinkedIn URL"
+            />
+            <label className="fieldset-label">Github URL</label>
+            <input
+              type="url"
+              name="github"
+              className="input w-full"
+              placeholder="Github URL"
+            />
+            <label className="fieldset-label">Resume URL</label>
+            <input
+              type="url"
+              name="resume"
+              className="input w-full"
+              placeholder="Resume URL"
+            />
+            <button className="btn btn-neutral mt-4">Apply</button>
+          </fieldset>
+        </form>
       </div>
     </div>
   );
