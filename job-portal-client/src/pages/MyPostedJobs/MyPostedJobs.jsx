8c6dcbe8cfbbdb27bec8a21ed119bcc88d0e6ee5
riponmondalbd <riponmondal149@gmail.com>
2025-02-01 16:19:38 +0600

    show my posted jobs and count  job application

--- job-portal-client/src/pages/MyPostedJobs/MyPostedJobs.jsx
+++ job-portal-client/src/pages/MyPostedJobs/MyPostedJobs.jsx
@@ -14,6 +14,31 @@ const MyPostedJobs = () => {
   return (
     <div>
       <h2 className="text-3xl">Posted Jobs: {jobs.length}</h2>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Job Title</th>
+              <th>Deadline</th>
+              <th>Application Count</th>
+              <th>Favorite Color</th>
+            </tr>
+          </thead>
+          <tbody>
+            {jobs.map((job, index) => (
+              <tr key={job._id}>
+                <th>{index + 1}</th>
+                <td>{job.title}</td>
+                <td>{job.applicationDeadline}</td>
+                <td>{job.applicationCount}</td>
+                <td>Blue</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
--- job-portal-server/index.js
+++ job-portal-server/index.js
@@ -84,6 +84,28 @@ async function run() {
     app.post("/job-applications", async (req, res) => {
       const applications = req.body;
       const result = await jobApplicationCollection.insertOne(applications);
+
+      // not the best way (use aggregate)
+      // skip --------> it
+      const id = applications.job_id;
+      const query = { _id: new ObjectId(id) };
+      const job = await jobsCollection.findOne(query);
+      let newCount = 0;
+      if (job.applicationCount) {
+        newCount = job.applicationCount + 1;
+      } else {
+        newCount = 1;
+      }
+
+      // now update the job info
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          applicationCount: newCount,
+        },
+      };
+      const updateResult = await jobsCollection.updateOne(filter, updatedDoc);
+
       res.send(result);
     });
   } finally {
