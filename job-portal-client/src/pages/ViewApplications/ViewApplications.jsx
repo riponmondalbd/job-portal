760174fdd6c463ed4c05d4f3e404dd26b40331e1
riponmondalbd <riponmondal149@gmail.com>
2025-02-01 21:21:05 +0600

    update job application status

--- job-portal-client/src/pages/ViewApplications/ViewApplications.jsx
+++ job-portal-client/src/pages/ViewApplications/ViewApplications.jsx
@@ -1,7 +1,36 @@
 import { useLoaderData } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const ViewApplications = () => {
   const applications = useLoaderData();
+
+  const handleStatusUpdate = (e, id) => {
+    console.log(e.target.value, id);
+
+    const data = {
+      status: e.target.value,
+    };
+    fetch(`http://localhost:5000/job-applications/${id}`, {
+      method: "PATCH",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.modifiedCount) {
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: "Job Status Update Successfully",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+  };
   return (
     <div>
       <div className="text-3xl">
@@ -13,8 +42,8 @@ const ViewApplications = () => {
               <tr>
                 <th></th>
                 <th>Email</th>
-                <th>Job</th>
-                <th>Favorite Color</th>
+                <th>Status</th>
+                <th>Update Status</th>
               </tr>
             </thead>
             <tbody>
@@ -23,7 +52,19 @@ const ViewApplications = () => {
                   <th>{index + 1}</th>
                   <td>{app.applicant_email}</td>
                   <td>Quality Control Specialist</td>
-                  <td>Blue</td>
+                  <td>
+                    <select
+                      onChange={(e) => handleStatusUpdate(e, app._id)}
+                      defaultValue={app.status || "Change Status"}
+                      className="select select-sm"
+                    >
+                      <option disabled={true}>Change Status</option>
+                      <option>Under Review</option>
+                      <option>Set Interview</option>
+                      <option>Hired</option>
+                      <option>Rejected</option>
+                    </select>
+                  </td>
                 </tr>
               ))}
             </tbody>
