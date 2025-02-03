0e113500bbfdf269b5f11822851c0a0f02b7b1f1
riponmondalbd <riponmondal149@gmail.com>
2025-02-03 08:55:37 +0600

    use cookie parser and set token to Httponly cookie

--- job-portal-server/index.js
+++ job-portal-server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
@@ -8,6 +9,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.4ylud.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -38,8 +40,14 @@ async function run() {
     // Auth related APIs
     app.post("/jwt", async (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, "secret", { expiresIn: "1h" });
-      res.send(token);
+      const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "1h" });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: false,
+          sameSite: "none",
+        })
+        .send({ success: true });
     });
 
     // Jobs related APIs
