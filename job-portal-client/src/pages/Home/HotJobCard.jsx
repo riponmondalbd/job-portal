7537b8982abfd53d12defeed798e0038d4df2842
riponmondalbd <riponmondal149@gmail.com>
2025-02-15 21:23:31 +0600

    display job details info in the job card

--- job-portal-client/src/pages/Home/HotJobCard.jsx
+++ job-portal-client/src/pages/Home/HotJobCard.jsx
@@ -1,5 +1,56 @@
+import { IoLocationSharp } from "react-icons/io5";
+import { LuDollarSign } from "react-icons/lu";
+
 const HotJobCard = ({ job }) => {
-  return <div></div>;
+  const {
+    title,
+    company,
+    company_logo,
+    requirements,
+    description,
+    location,
+    salaryRange,
+  } = job;
+  return (
+    <div className="card bg-base-100 shadow-sm">
+      <div className="flex gap-2 m-2">
+        <figure>
+          <img className="w-16" src={company_logo} alt={company} />
+        </figure>
+        <div>
+          <h4 className="text-2xl">{company}</h4>
+          <p className="flex items-center gap-1">
+            <IoLocationSharp /> {location}
+          </p>
+        </div>
+      </div>
+      <div className="card-body">
+        <h2 className="card-title">
+          {title}
+          <div className="badge badge-secondary">NEW</div>
+        </h2>
+        <p>{description}</p>
+        <div className="flex gap-2 flex-wrap">
+          {requirements.map((skill, index) => (
+            <p
+              className="border rounded text-center px-2 hover:text-blue-500"
+              key={index}
+            >
+              {skill}
+            </p>
+          ))}
+        </div>
+        <div className="card-actions">
+          <p className="flex items-center">
+            Salary: <LuDollarSign /> {salaryRange.min} - {salaryRange.max}{" "}
+            {salaryRange.currency}
+          </p>
+
+          <button className="btn btn-primary w-full">View Details</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HotJobCard;
--- job-portal-client/src/pages/Home/HotJobs.jsx
+++ job-portal-client/src/pages/Home/HotJobs.jsx
@@ -14,7 +14,7 @@ const HotJobs = () => {
   }, []);
   return (
     <div>
-      <div>
+      <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {jobs.map((job) => (
           <HotJobCard key={job._id} job={job}></HotJobCard>
         ))}
