941b8ff9e63aa16b8cb712dd036cfce23c984a65
riponmondalbd <riponmondal149@gmail.com>
2025-02-16 00:28:07 +0600

    use custom hook and created job application post api

--- job-portal-client/src/pages/JobApply/JobApply.jsx
+++ job-portal-client/src/pages/JobApply/JobApply.jsx
@@ -1,8 +1,11 @@
 import { useParams } from "react-router";
+import useAuth from "../../hooks/useAuth";
 
 const JobApply = () => {
   const { id } = useParams();
-  console.log(id);
+  const { user } = useAuth();
+
+  //   console.log(id, user);
 
   const submitJobApplication = (e) => {
     e.preventDefault();
@@ -11,48 +14,47 @@ const JobApply = () => {
     const github = form.github.value;
     const resume = form.resume.value;
 
-    console.log(linkedin, github, resume);
+    // console.log(linkedin, github, resume);
+
+    const jobApplication = {
+      job_id: id,
+      applicant_email: user.email,
+      linkedin,
+      github,
+      resume,
+    };
   };
   return (
-    <div className="hero bg-base-200 min-h-screen">
-      <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="text-center lg:text-left">
-          <h1 className="text-5xl font-bold">Login now!</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-        </div>
-        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-          <div className="card-body">
-            <form onSubmit={submitJobApplication} className="fieldset">
-              <label className="fieldset-label">LinkedIn URL</label>
-              <input
-                type="url"
-                name="linkedin"
-                className="input"
-                placeholder="LinkedIn URL"
-              />
-              <label className="fieldset-label">Github URL</label>
-              <input
-                type="url"
-                name="github"
-                className="input"
-                placeholder="Github URL"
-              />
-              <label className="fieldset-label">Resume URL</label>
-              <input
-                type="url"
-                name="resume"
-                className="input"
-                placeholder="Resume URL"
-              />
+    <div className="card bg-base-100 w-full shadow-2xl">
+      <h1 className="text-5xl font-bold text-center mt-4">
+        Apply Job And Good Luck!
+      </h1>
+      <div className="card-body">
+        <form onSubmit={submitJobApplication} className="fieldset">
+          <label className="fieldset-label">LinkedIn URL</label>
+          <input
+            type="url"
+            name="linkedin"
+            className="input w-full"
+            placeholder="LinkedIn URL"
+          />
+          <label className="fieldset-label">Github URL</label>
+          <input
+            type="url"
+            name="github"
+            className="input w-full"
+            placeholder="Github URL"
+          />
+          <label className="fieldset-label">Resume URL</label>
+          <input
+            type="url"
+            name="resume"
+            className="input w-full"
+            placeholder="Resume URL"
+          />
 
-              <button className="btn btn-neutral mt-4">Apply</button>
-            </form>
-          </div>
-        </div>
+          <button className="btn btn-neutral mt-4">Apply</button>
+        </form>
       </div>
     </div>
   );
--- job-portal-server/index.js
+++ job-portal-server/index.js
@@ -31,6 +31,9 @@ async function run() {
 
     // jobs related apis
     const jobsCollection = client.db("jobPortal").collection("jobs");
+    const jobApplicationCollection = client
+      .db("jobPortal")
+      .collection("job_applications");
 
     app.get("/jobs", async (req, res) => {
       const result = await jobsCollection.find().toArray();
@@ -41,6 +44,13 @@ async function run() {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await jobsCollection.findOne(query);
+      res.send(result);
+    });
+
+    // job application apis
+    app.post("/job-applications", async (req, res) => {
+      const application = req.body;
+      const result = await jobApplicationCollection.insertOne(application);
       res.send(result);
     });
   } finally {
