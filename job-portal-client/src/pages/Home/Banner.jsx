ec2dadd1cdd9522d7d63687fb154c85743f4a8e7
riponmondalbd <riponmondal149@gmail.com>
2025-02-15 15:40:47 +0600

    banner image animation using framer motion

--- job-portal-client/src/pages/Home/Banner.jsx
+++ job-portal-client/src/pages/Home/Banner.jsx
@@ -1,13 +1,26 @@
 import { easeOut } from "motion";
 import { motion } from "motion/react";
+import team1 from "../../assets/team/office-happy-team.jpg";
+import team2 from "../../assets/team/programming -team-celebrate.jpg";
+
 const Banner = () => {
   return (
     <div className="hero bg-base-200 min-h-96">
       <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="flex-1">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-            className="max-w-sm rounded-lg shadow-2xl"
+          <motion.img
+            src={team1}
+            alt="office happy team"
+            animate={{ y: [50, 100, 50] }}
+            transition={{ duration: 10, repeat: Infinity }}
+            className="max-w-sm rounded-t-[40px] rounded-br-[40px] border-l-4 border-b-4 border-blue-500 w-64 shadow-2xl"
+          />
+          <motion.img
+            src={team2}
+            alt="programming team celebration"
+            animate={{ x: [100, 150, 100] }}
+            transition={{ duration: 10, repeat: Infinity, delay: 5 }}
+            className="max-w-sm rounded-t-[40px] rounded-br-[40px] border-l-4 border-b-4 border-blue-500 w-64 shadow-2xl"
           />
         </div>
         <div className="flex-1">
@@ -21,7 +34,14 @@ const Banner = () => {
             }}
             className="text-5xl font-bold"
           >
-            Latest Jobs for Your!
+            Latest{" "}
+            <motion.span
+              animate={{ color: ["#0ba32f", "#a3320b", "#1b0ba3"] }}
+              transition={{ duration: 1.5, repeat: Infinity }}
+            >
+              Jobs
+            </motion.span>{" "}
+            for Your!
           </motion.h1>
           <p className="py-6">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
