d00b09da572b830ac5a577d4ce249b2a1069c60a
riponmondalbd <riponmondal149@gmail.com>
2025-02-16 16:01:48 +0600

    create large form with select for add form

--- job-portal-client/src/pages/AddJob/AddJob.jsx
+++ job-portal-client/src/pages/AddJob/AddJob.jsx
@@ -19,16 +19,85 @@ const AddJob = () => {
           className="input w-full"
           placeholder="Job Location"
         />
-        {/* job location */}
-        <label className="fieldset-label">Job Location</label>
+        {/* job type */}
+        <label className="fieldset-label">Job Type</label>
+        <select defaultValue="Pick a Job type" className="select w-full">
+          <option disabled={true}>Pick a Job type</option>
+          <option>Full-time</option>
+          <option>Intern</option>
+          <option>Part-time</option>
+        </select>
+        {/* job category */}
+        <label className="fieldset-label">Job Category</label>
+        <select defaultValue="Pick a Job Category" className="select w-full">
+          <option disabled={true}>Pick a Job Category</option>
+          <option>Engineering</option>
+          <option>Marketing</option>
+          <option>Finance</option>
+          <option>Teaching</option>
+        </select>
+        {/* salary range */}
+        <label className="fieldset-label">Salary Range</label>
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+          <div>
+            <input
+              type="number"
+              name="min"
+              className="input w-full"
+              placeholder="Min"
+            />
+          </div>
+          <div>
+            <input
+              type="number"
+              name="max"
+              className="input w-full"
+              placeholder="Max"
+            />
+          </div>
+          <div>
+            <select defaultValue="Currency" className="select w-full">
+              <option disabled={true}>Currency</option>
+              <option>BDT</option>
+              <option>USD</option>
+              <option>INR</option>
+            </select>
+          </div>
+        </div>
+        {/* job description */}
+        <label className="fieldset-label">Job Description</label>
+        <textarea
+          className="textarea w-full"
+          placeholder="Job Description"
+          name="description"
+          required
+        ></textarea>
+        {/* company name */}
+        <label className="fieldset-label">Company Name</label>
         <input
           type="text"
-          name="location"
+          name="company"
           className="input w-full"
-          placeholder="Job Location"
+          placeholder="Company Name"
         />
-
-        <button className="btn btn-neutral mt-4">Login</button>
+        {/* requirements */}
+        <label className="fieldset-label">Job Requirements</label>
+        <textarea
+          className="textarea w-full"
+          placeholder="Put each Requirements in a one line"
+          name="requirements"
+          required
+        ></textarea>
+        {/* responsibilities */}
+        <label className="fieldset-label">Job Responsibilities</label>
+        <textarea
+          className="textarea w-full"
+          placeholder="Put each Responsibilities in a one line"
+          name="responsibilities"
+          required
+        ></textarea>
+        {/* submit button */}
+        <button className="btn btn-neutral mt-4">Submit</button>
       </form>
     </div>
   );
